85ed98937a3de1087766846d808937ca6a03905a
mjeronen <mjeronen@hireright.com>
2025-03-05 19:39:29 +0200

    Initial commit.

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		displayGreeting();
+		
+		Sum myData = new Sum();
+		myData.display();
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("Greetings!");
+	}
+	
+}
---
+++ HelloWorld/src/Sum.java
@@ -0,0 +1,6 @@
+public class Sum {
+	int data[] = {2, 4, 6, 8};
+	public void display() {
+		System.out.println("Sum = ");
+	}
+}
